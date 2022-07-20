6c8fda8b5dbbba8c34d358e6b0f347fe8685c3ab
ksouth <katesoutherland8@gmail.com>
2022-07-20 11:56:24 -0700

    Rendering current data

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,10 +3,12 @@ import React, { useState, useEffect } from 'react';
 
 import locationData from './locations.json';
 
+const urlBase = 'https://flexweather.com/api/'
 
 function List({ currentData, setCurrentData }) {
 
-    useEffect(() => {})
+    useEffect(() => {
+        const url = `${urlBase}now?lat=45.714848&lon=-121.516733&units=imperial`;
     
     const myHeaders = new Headers();
         myHeaders.append("Accept", "application/json");
@@ -17,10 +19,16 @@ function List({ currentData, setCurrentData }) {
         redirect: 'follow'
     };
 
-    // fetch("https://flexweather.com/api/now?lat=45.714848&lon=-121.516733&units=imperial", requestOptions)
-        // .then(response => response.text())
-        // .then(result => console.log(result))
-        // .catch(error => console.log('error', error));
+    fetch(url, requestOptions)
+        .then(response => response.json())
+        .then((result) => {
+            console.log(result)
+            let newCurrentData = result.wind_speed;
+            console.log(newCurrentData)
+            setCurrentData(newCurrentData);
+        })
+        .catch(error => console.log('error', error));
+    })
     
     let list = locationData.map(item => {
         return (
@@ -37,6 +45,7 @@ function List({ currentData, setCurrentData }) {
     return(
         <>
             <h2>How's the wind today?</h2>
+            <p>Current Windspeed: {currentData}</p>
             {list}
         </>
     );
